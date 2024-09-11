3188da17f3423793f5f9180d8ac3993636a38b3c
Chloe-Ms <mllechloe.mathias@gmail.com>
2024-09-11 15:29:51 +0200

    [MOD] Change player forward mvt to world mvt

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    public static ForwardWorldMovement WorldMovement { get; set; }
+}
