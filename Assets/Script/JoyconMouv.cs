cb82cd7fc6bbcd418b3b2f618491e6a51bb99f89
Mathieu Guitter <guittermathieu@gmail.com>
2024-09-11 11:23:37 +0200

    [ADD] JoyconMouv

---
+++ Assets/Script/JoyconMouv.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyconMouv : MonoBehaviour
+{
+    private List<Joycon> joycons;
+
+    public Quaternion orientation;
+    public Vector3 gyro;
+    public int jc_ind = 0;
+    public Vector3 defaultPosition;
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    private Vector3 newPosition;
+    private Vector3 realPosition;
+    
+    void Start()
+    {
+        gyro = new Vector3(0, 0, 0);
+        realPosition = gameObject.transform.position;
+        // get the public Joycon array attached to the JoyconManager in scene
+        joycons = JoyconManager.Instance.j;
+		if (joycons.Count < jc_ind+1){
+			Destroy(gameObject);
+		}
+    }
+
+    
+    void Update()
+    {
+        if (joycons.Count > 0)
+        {
+            Joycon j = joycons [jc_ind];
+            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+            {
+                //Debug.Log ("Shoulder button 2 pressed");
+                realPosition = new Vector3(0,0,0);
+            }
+            gyro = j.GetGyro();
+            realPosition.x += (gyro.x / 5);
+            if (realPosition.x < minX)
+            {
+                newPosition.x = minX;
+                Debug.Log("Min");
+                Debug.Log(realPosition.x);
+            }else if (realPosition.x > maxX)
+            {
+                newPosition.x = maxX;
+                Debug.Log("Max");
+                Debug.Log(realPosition.x);
+            }else
+            {
+                newPosition.x = realPosition.x;
+                Debug.Log("Shit");
+                Debug.Log(realPosition.x);
+            }
+            gameObject.transform.position = newPosition;
+            Debug.Log(gyro);
+        }
+    }
+}
