705677ae1d2514ea912c81d6331e63449e0d466b
Mathieu Guitter <guittermathieu@gmail.com>
2024-09-12 17:19:09 +0200

    [MOD] JoyconMouv

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -17,22 +17,27 @@ public class Timer : MonoBehaviour
         {
             case true:
                 if (remainingTime > 0)
-        {
-            remainingTime -= Time.deltaTime;
-        }
-        else if (remainingTime < 0)
-        {
-            remainingTime = 0;
-            // GameOver
-            timerText.color = Color.red;
-            gameOverPanel.SetActive(true);
-            Time.timeScale = 0;
-        }
-
-
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                {
+                    remainingTime -= Time.deltaTime;
+                    if (remainingTime < 10)
+                    {
+                        timerText.color = Color.red;
+                    }
+                    else if (remainingTime < 50)
+                    {
+                        timerText.color = Color.yellow;
+                    }
+                }
+                else if (remainingTime < 0)
+                {
+                    remainingTime = 0;
+                    // GameOver
+                    gameOverPanel.SetActive(true);
+                    Time.timeScale = 0;
+                }
+                int minutes = Mathf.FloorToInt(remainingTime / 60);
+                int seconds = Mathf.FloorToInt(remainingTime % 60);
+                timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
                 break;
         }
 
