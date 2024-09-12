2cfab1984d537c7eacf7c3addecd26b530dd3f28
Mathieu Guitter <guittermathieu@gmail.com>
2024-09-12 11:56:48 +0200

    [MOD] Timer
    death panel enable at 00:00

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -8,6 +8,7 @@ public class Timer : MonoBehaviour
     
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField]float remainingTime;
+    public GameObject gameOverPanel;
     void Update()
     {
         if (remainingTime > 0)
@@ -19,7 +20,11 @@ public class Timer : MonoBehaviour
             remainingTime = 0;
             // GameOver
             timerText.color = Color.red;
+            gameOverPanel.SetActive(true);
+            Time.timeScale = 0;
         }
+
+
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
