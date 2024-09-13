db473008d25db37dd1b98c3fb3e3a8e4b917b1dc
Mathieu Guitter <guittermathieu@gmail.com>
2024-09-13 09:25:14 +0200

    [MOD] TriggerStartBoost
    add sfx

--- Assets/Scripts/TriggerStartBoost.cs
+++ Assets/Scripts/TriggerStartBoost.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class TriggerStartBoost : MonoBehaviour
 {
+    AudioManager audioManager;
+
+    public void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other != null && other.gameObject.tag == "Player")
         {
             Managers.WorldMovement.StartBoost();
+            audioManager.PlaySFX(audioManager.boostSFX);
         }
     }
 }
